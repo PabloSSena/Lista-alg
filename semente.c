c2ca89028a16e69f059c0a664f75e8b993d9d259
PabloSSena <58041720+PabloSSena@users.noreply.github.com>
2019-11-30 18:24:33 -0300

    Add files via upload

--- semente.c
+++ semente.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 
 int main(){
-  int f,r,gt1,gt2,cont =0,para,i,teste=0;
+  int f,r,gt1,gt2,cont=0,para=0,i=0,teste=0,num=0,soma=0,j,k;
+
   FILE *arq;
 
   arq = fopen("entrada.txt","r");
@@ -10,36 +11,50 @@ int main(){
      printf("Deu erro\n");
 
   else{
-     printf("Deu certo\n");
      fscanf(arq,"%d %d %d %d",&f,&r,&gt1,&gt2);
 
-     printf("%d %d %d %d\n",f,r,gt1,gt2);
+  if(f >=1 && f <= 100000 && r >=1 && r <= 1000){
+         gt1 -= 1;
+         gt2 -= 1;
 
-     int fita[f];
-     fita[gt1] = 1;
-     fita[gt2] = 1;
+         int fita[f];
 
-     do{
-        for(i = 0;i < f; i++){
-            if(fita[i] != 1){
-                if(gt1 - i > 0 || gt1 + i < f){
-                   fita[gt1-i] = 1;
-                   fita[gt2+i] = 1;
-                }
-                if(gt2 - i > 0 || gt2 + i < f){
-                   fita[gt1-i] = 1;
-                   fita[gt2+i] = 1;
-                }
-            }
-            else
-                teste++;
+         fita[gt1] = 1;
+         fita[gt2] = 1;
 
-            cont++;
-        }
+         for(i=0;i<f;i++){
+             soma = 0;
+             if(gt1+i < f)
+                fita[gt1+i] = 1;
 
-    }
-    while(cont == 0);
+             if(gt1-i >= 0)
+                fita[gt1-i] = 1;
 
-    printf("Dias = %d\n",cont);
+             if(gt2+i < f)
+                fita[gt2+i] = 1;
+
+             if(gt2-i >= 0)
+                fita[gt2-i] = 1;
+
+
+             for(j=0;j<f;j++){
+                 num = fita[j];
+                 soma += num;
+                 if(soma == f && para == 0){
+                    cont = i;
+                    para++;
+                  }
+              }
+         }
+
+        printf("\n%d\n",cont);
+      }
+  else
+    printf("Erro!\n");
+  }
+  fclose(arq);
 }
-}
+
+
+
+
