c97057c4d91e2e8a06e373aa8cad9ad0a5ba8627
PabloSSena <58041720+PabloSSena@users.noreply.github.com>
2019-12-01 02:59:38 -0300

    Add files via upload

---
+++ Forca.c
@@ -0,0 +1,123 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#define N 100
+
+
+
+int main(){
+  char tent,tentM,tentN,c,ini[N];
+  int chan=6,tam,op,i,pos,cont=0,gan = 0,perd = 0,gan2 = 0;
+
+  FILE *sec;
+
+  sec = fopen("segredo.txt","r");
+
+  if(sec == NULL)
+     printf("Deu erro\n");
+
+
+  else{
+      fscanf(sec," %[^\n]",ini);
+      tam = strlen(ini);
+
+      char seg[tam],tenta[tam],acertos[tam],erros[6];
+
+      strcpy(seg,ini);
+
+       printf("\t\t\tNumero de letras = %d\n",tam);
+       printf("\t\t\tDica = Um lugar\n\n");
+
+       for(i=0;i<6;i++)
+           erros[i] = '*';
+
+       for(i=0;i<tam;i++)
+           acertos[i] = '_';
+
+    do{
+       //Printando os * e letras//
+       printf("Erros:");
+       for(i=0;i<6;i++)
+           printf(" %c",erros[i]);
+
+        //desenhos//
+       if(chan == 6)
+          printf("\n |==|\n |\n |\n |\n |\n/ \\");
+
+       if(chan == 5)
+         printf("\n |==|\n |  o\n |\n |\n |\n/ \\");
+
+       if(chan == 4)
+         printf("\n |==|\n |  o\n |  |\n |\n |\n/ \\");
+
+       if(chan == 3)
+         printf("\n |==|\n |  o\n | /|\n |\n |\n/ \\");
+
+       if(chan == 2)
+         printf("\n |==|\n |  o\n | /|\\\n |\n |\n/ \\");
+
+       if(chan == 1)
+         printf("\n |==|\n |  o\n | /|\\\n | /\n | \n/ \\");
+
+       //Printando os _ e letras//
+       for(i=0;i<tam;i++)
+           printf(" %c",acertos[i]);
+
+
+       printf("\n1 - Para tentar uma letra\n2 - Para tentar a frase\n");
+       scanf("%d",&op);
+
+         switch(op){
+
+           case 1:printf("\nLetra = ");
+                  scanf(" %c",&tent);
+
+                  tentM = toupper(tent);
+                  tentN = tolower(tent);
+                  cont = 0;
+
+                  for(i=0;i<tam;i++){
+                     if(seg[i] == tent||seg[i] == tentM||seg[i] == tentN){
+                        pos = i;
+                        cont++;
+                        acertos[pos] = tent;
+                     }
+                  }
+                  if(cont == 0){
+                     erros[chan-1] = tent;
+                     chan--;
+                  }
+
+                  if(strcmpi(acertos,seg) == 1)
+                     gan2 = 1;
+
+                  printf("\n\n");
+                  break;
+
+            case 2:printf("\nDigite a frase = ");
+                   scanf(" %[^\n]",tenta);
+
+                   if(strcmpi(tenta,seg) == 0)
+                      gan = 1;
+
+                   else
+                      perd = 1;
+
+                  break;
+         }
+    }
+     while(gan == 0 && perd == 0 && chan != 0 && gan2 == 0);
+
+     if(gan == 1 || gan2 == 1){
+        printf("\n!!!!!!!!!!Parabens voce ganhou!!!!!!!!!!\n!!!!!!!!!!a palavra era: %s!!!!!!!!!!",seg);
+        return 0;
+     }
+
+     if(perd == 1 || chan == 0)
+        printf("\n |==|\n |  o\n | /|\\\n | / \\\n | \n/ \\");
+        printf("\t\tPerdeste\n");
+        printf("\t\tA palavra era: %s",seg);
+
+  }
+}
+
